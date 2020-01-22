73f1d05585dcaa99ce7c9d97ae225db95abd662e
MariusDoe <github.marius@doerbandt.de>
2020-01-22 09:35:35 +0100

    added Singleton, add(hitBox) and remove(hitBox) and _physics(dt) as well as the DT constant

--- CollisionManager.py
+++ CollisionManager.py
@@ -1,7 +1,31 @@
-class CollisionManager:
+DT = 1 / 60
 
-    def __init__(self):
-        self.hitboxes = []
+class CollisionManager(object):
 
-    def update(self, dt):
-        pass # TODO
+    class _CollisionManager:
+
+        def __init__(self):
+            self.hitBoxes = set()
+            self.acc = 0
+
+        def add(self, hitBox):
+            self.hitBoxes.add(hitBox)
+
+        def remove(self, hitBox):
+            self.hitBoxes.remove(hitBox)
+
+        def update(self, dt):
+            self.acc += dt
+            while self.acc >= DT:
+                self._physics(DT)
+                self.acc -= DT
+
+        def _physics(self, dt):
+            pass
+
+    instance = None
+
+    def __new__(cls):
+        if not cls.instance:
+            cls.instance = CollisionManager._CollisionManager()
+        return cls.instance
