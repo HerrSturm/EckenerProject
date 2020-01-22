d8ff6c3d147c96dbb3b9d572914ff5694f7f960d
bluppus <g4fl1p@gmail.com>
2020-01-22 10:11:28 +0100

    update function and move function added

--- blockClass.py
+++ blockClass.py
@@ -9,8 +9,16 @@ class block(object):
         super(block, self).__init__()
         self.lenght = lenght*24
         self.height = height*24
-        self.position = position
+        self.position = (position[0]*24,position[1]*24)
         self.color = color
         self.screen = pygame.display.get_surface()
         #self.hitbox = Hitbox(self, (self.lenght, self.height), True)
         pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.lenght, self.height])
+
+    def update(self):
+        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.lenght, self.height])
+
+    def move(self,position):
+        self.position = (self.position+position[0]*24,self.position+postition[1]*24)
+        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.lenght, self.height])
+        
