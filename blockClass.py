cb1ecd12c4959d399222e531d1948a258b81b027
bluppus <g4fl1p@gmail.com>
2020-01-22 10:29:30 +0100

    Changed to vectors

--- blockClass.py
+++ blockClass.py
@@ -3,23 +3,21 @@
 #Ole, Philippe
 
 import sys, pygame
+from HitBox import *
 
 class block(object):
-    def __init__(self, lenght, height, position, color):
+    def __init__(self, size, position, color):
         super(block, self).__init__()
-        self.lenght = lenght*24
-        self.height = height*24
-        self.position = (position[0]*24,position[1]*24)
+        self.size = Vec2(size[0]*24,size[1]*24)
+        self.position = Vec2(position[0]*24,position[1]*24)
         self.color = color
         self.screen = pygame.display.get_surface()
-        #self.hitbox = Hitbox(self, (self.lenght, self.height), True)
-        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.lenght, self.height])
+        self.hitbox = HitBox(Vec2(self.position[0],self.position[1])),Vec2(self.length,self.height), True, Layer("platform"))
+        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.size])
 
     def update(self):
-        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.lenght, self.height])
+        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.size])
 
     def move(self,position):
         self.position = (self.position+position[0]*24,self.position+postition[1]*24)
-        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.lenght, self.height])
-
-        
+        pygame.draw.rect(self.screen, self.color, [self.position[0],self.position[1], self.size])
