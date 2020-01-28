7fd6b60221ac06404af0cfbc02c2911761e42b33
MariusDoe <github.marius@doerbandt.de>
2020-01-28 11:25:11 +0100

    rewrote system to use a dict() of frozensets

--- Layer.py
+++ Layer.py
@@ -27,31 +27,26 @@ class LayerMap:
 
     def __init__(self):
         self.map = dict()
+        self.trackedNames = set()
         for name in Layer.names:
             self.add(name)
         Layer.onNewLayer(lambda name: self.add(name))
 
     def add(self, name):
-        for key in self.map.keys():
-            self.map[key][name] = True
-        self.map[name] = dict()
+        if name in self.trackedNames:
+            return
+        self.trackedNames.add(name)
+        for tracked in self.trackedNames:
+            self.map[frozenset((name, tracked))] = True
 
     def matches(self, name0, name1):
         if type(name0) is Layer: name0 = name0.name
         if type(name1) is Layer: name1 = name1.name
-        if name0 not in self.map or name1 not in self.map:
-            return False
-        return ((name1 in self.map[name0] and self.map[name0][name1]) or
-                (name0 in self.map[name1] and self.map[name1][name0]))
+        return self.map[frozenset((name0, name1))]
 
     def setMatch(self, name0, name1, match):
         if type(name0) is Layer: name0 = name0.name
         if type(name1) is Layer: name1 = name1.name
-        if name0 not in self.map or name1 not in self.map:
-            return
-        if name1 in self.map[name0]:
-            self.map[name0][name1] = match
-            return
-        if name0 in self.map[name1]:
-            self.map[name1][name0] = match
-            return
+        if name0 not in self.trackedNames: return
+        if name1 not in self.trackedNames: return
+        self.map[frozenset((name0, name1))] = match
