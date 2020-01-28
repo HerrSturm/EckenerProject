2d4eeb8801f20c6449274a996a0ab690ac088c5d
info-Fabius <fabsius2003@gmail.com>
2020-01-28 10:59:22 +0100

    Move Funktion (Rechts,Links, stehen bleiben)

--- MoveFunktion1.py
+++ MoveFunktion1.py
@@ -1,13 +1,18 @@
 from HitBox import *
 #playerHitBox = HitBox(vec2(100,100),vec2(100,100),False,Layer("player"),vec2(0,0))
 
-def moveright(self):
+def moveright(self):                            #Funktion um die Hitbox nach rechts zu bewegen (geschw. auf +1)
     playerHitBoxVel = self.vel()
     if playerHitBoxVel <= 10:
-        playerHitBox.vel = vec2(playerHitBoxVel+1,0)       #hitbox bewegt sich nach rechts
+        playerHitBox.vel = vec2(1,0)            #hitbox bewegt sich nach rechts
 
 
-def moveleft(self):
+def moveleft(self):                             #Funktion um die Hitbox nach links zu bewegen (geschw. auf -1)
     playerHitBoxVel = self.vel()
     if playerHitBoxVel >= -10:
-        playerHitBox.vel = vec2 (playerHitBoxVel-1,0)      #hitbox bewegt sich nach links
+        playerHitBox.vel = vec2 (-1,0)          #hitbox bewegt sich nach links
+
+
+def standstill(self):                           #Funktion um die Hitbox zum stehen zu bringen (geschw. auf 0)
+    playerHitBoxVel = self.vel()
+    playerHitBox.Vel = vec2 (0,0)               #Hitbox bleibt stehen
