ec5c9193f8bd968f36a60c9339101bb546289888
Cinnay1621 <yannicbasin@yahoo.de>
2020-02-12 10:33:52 +0100

    cleaned up code and added jump and move

--- CharacterClass.py
+++ CharacterClass.py
@@ -2,30 +2,37 @@ import pygame
 from HitBox import*
 #CONST gravity
 class Character():
+    GRAVITY = 1
+    JUMPVEL = 100
+    MOVEVEL = 10
     def __init__(self, position): #Vec2 position
         self.isGrounded = False
-        self.GRAVITY = 10
-        self.JUMPVEL = 100
-        self.vel = Vec2(0,0)
         size = Vec2(150,150)
-        self.hitBox = HitBox(position, size, False, Layer("player"),vel)
-        self.hitBox.onCollide(check_Grounded)
-        CollisionManager().beforeUpdate(beforeCollisionManager)
-        CollisionManager().afterUpdate(afterCollisionManager)
+        self.hitBox = HitBox(position, size, False, Layer("player"),Vec2(0,0))
+        self.hitBox.onCollide(self.check_Grounded)
+        CollisionManager().onBeforeUpdate(self.beforeCollisionManager)
+        CollisionManager().onAfterUpdate(self.afterCollisionManager)
         self.mainScreen = pygame.display.get_surface()
-        pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
 
+    #checks if hitbox collided with ground
     def check_Grounded(self, other, dir):
         if dir == Direction.DOWN:
             self.isGrounded = True
 
-    def update(self, dt):
-        pygame.draw.rect(self.mainScreen, self.shape)
+    #draws the character on the screen
+    def draw(self):
+        pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
 
+    #updates the player
+    def update(self, dt):
+        self.draw()
+
+    #gets called before the collisionmanager does stuff
     def beforeCollisionManager():
         self.isGrounded = False
         self.vel += Vec2(0, -self.GRAVITY*dt)
 
+    #gets called after the collisionmanager did stuff
     def afterCollisionManager():
         #after col update
         self.testGrounded = False
@@ -33,3 +40,17 @@ class Character():
             self.vel.y = 0
             if jump: #jump command given
                 self.vel += Vec2(0, self.JUMPVEL)
+
+    def moveright(self):                           #Funktion um die Hitbox nach rechts zu bewegen (geschw. auf +1)
+        self.hitBox.vel = Vec2(self.MOVEVEL,0)            #hitbox bewegt sich nach rechts
+
+    def moveleft(self):                             #Funktion um die Hitbox nach links zu bewegen (geschw. auf -1)
+        self.hitBox.vel = Vec2(-self.MOVEVEL,0)          #hitbox bewegt sich nach links
+
+    def standstill(self):                           #Funktion um die Hitbox zum stehen zu bringen (geschw. auf 0)
+        self.hitBox.vel = Vec2(0,0)               #Hitbox bleibt stehen
+
+    #makes the player jump:only when grounded
+    def jump(self):
+        if isGrounded:
+            self.vel += Vec2(0, self.JUMPVEL)
