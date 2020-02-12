3b0a66e9817bfc817db6d751c13f228255c78b9f
info-Fabius <fabsius2003@gmail.com>
2020-02-12 20:03:38 +1200

    Gegner Klasse erstellt

---
+++ GegnerClass.py
@@ -0,0 +1,17 @@
+import pygame, HitBox, Vec2
+from HitBox import *
+class Gegner(object):
+    def __init__(self, pos,startRange, endRange):
+        self.hitBox = HitBox(pos, Vec2(100,100), False, Layer("player"))
+        self.startRange = startRange
+        self.endRange = endRange
+        self.mainScreen = pygame.display.get_surface()
+    def draw(self):
+        pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
+
+    def move(self):
+        if self.startRange < self.endRange:
+            if self.hitBox.pos.x < self.startRange:
+                self.hitBox.vel.x = 100
+            if self.hitBox.pos.x > self.endRange:
+                self.hitBox.vel.x = -100
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import sys, pygame, characterClass
+import sys, pygame, characterClass, HitBox, Vec2
+from GegnerClass import *
 pygame.init()
 mainScreenSize = width, height = 1280, 720
 mainScreen = pygame.display.set_mode(mainScreenSize)
 character=characterClass.Character()
+gegner = Gegner(Vec2(50,50),80, 800)
+clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     keys = pygame.key.get_pressed()
+    mainScreen.fill(pygame.Color(0,0,0))
+    gegner.draw()
+    gegner.move()
     pygame.display.flip()
     #Shutdownhandling
     if keys[pygame.K_ESCAPE]:
         sys.exit()
+
+    dt = clock.get_time() / 1000.0
+    CollisionManager().update(dt)
