10bd9878c1671264959df83d1d7dabebf082f801
bluppus <g4fl1p@gmail.com>
2020-02-18 11:44:17 +0100

    Switched position/size order

--- Block.py
+++ Block.py
@@ -7,10 +7,10 @@ from Vec2 import Vec2
 from HitBox import *
 
 class Block(object):
-    def __init__(self, size, position, color):
+    def __init__(self, position, size, color):
         super(Block, self).__init__()
-        self.size = size * 24
         self.position = position * 24
+        self.size = size * 24
         self.color = color
         self.screen = pygame.display.get_surface()
         self.hitbox = HitBox(self.position,self.size, True, Layer("solid"))
