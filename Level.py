e901eb79031f98126207a7c2b5521c236ef955c6
MariusDoe <github.marius@doerbandt.de>
2020-02-18 03:00:12 -0700

    initial commit

---
+++ Level.py
@@ -0,0 +1,29 @@
+colors = {
+    "brown": (150,80,50),
+    "blue": (80,150,255),
+    "green": (50,100,50)
+}
+
+class Level:
+
+    def __init__(self, objects, characterSpawn):
+        self.objects = objects
+        self.character = Character() # TODO: use characterSpawn
+        self.objects.append(self.character)
+
+    def load(json):
+        objects = []
+        for object in level["objects"]:
+            if object["type"] == "block":
+                objects.append(Block(
+                    Vec2(*object["position"]),
+                    Vec2(*object["size"]),
+                    colors[object["color"]]
+                ))
+            if object["type"] == "enemy":
+                objects.append(Gegner(
+                    Vec2(*object["position"]),
+                    Vec2(*object["size"]),
+                    object["range"][0]
+                    object["range"][1]
+                ))
