895bbcadc485a164321696f7ec5922e7b750127e
bluppus <g4fl1p@gmail.com>
2020-02-25 11:06:42 +0100

    Umlaute aus Kommentaren entfernt

--- tester.py
+++ tester.py
@@ -1,11 +1,12 @@
 import sys, pygame, time
 from Block import Block
 from Vec2 import Vec2
-from characterClass import Character
+from Character import Character
 from CollisionManager import CollisionManager
+from GegnerClass import *
 pygame.init()
 
-# Variabeln für Farben werden kreiert
+# Variabeln fuer Farben werden kreiert
 brown = (150,80,50)
 blue = (80,150,255)
 green = (50,100,50)
@@ -18,27 +19,33 @@ pygame.display.flip()
 
 screen.fill((80,150,255))
 
-# Alle Blöcke werden mit Größe, Position und Farbe kreiert (Vec2)
-b1 = Block(Vec2(0,26), Vec2(10, 10), brown)
-b1_2 = Block(Vec2(0,25), Vec2(10, 1), green)
-b2 = Block(Vec2(14,22), Vec2(10, 20), brown)
-b2_2 = Block(Vec2(14,21), Vec2(10, 1), green)
-b3 = Block(Vec2(28,19), Vec2(5, 1), brown)
-b3_2 = Block(Vec2(28,18), Vec2(5, 1), green)
-b4 = Block(Vec2(37,23), Vec2(30, 20), brown)
-b4_2 = Block(Vec2(37,22), Vec2(30, 1), green)
-b5 = Block(Vec2(44, 20), Vec2(6, 3), brown)
-b5_2 = Block(Vec2(44, 19), Vec2(6, 1), green)
+# Alle Bloecke werden mit Groesse, Position und Farbe kreiert (Vec2)
+b1 = Block(Vec2(10, 10), Vec2(0,26), brown)
+b1_2 = Block(Vec2(10, 1), Vec2(0,25), green)
+b2 = Block(Vec2(10, 20), Vec2(14,22), brown)
+b2_2 = Block(Vec2(10, 1), Vec2(14,21), green)
+b3 = Block(Vec2(5, 1), Vec2(28,19), brown)
+b3_2 = Block(Vec2(5, 1), Vec2(28,18), green)
+b4 = Block(Vec2(30, 20), Vec2(37,23), brown)
+b4_2 = Block(Vec2(30, 1), Vec2(37,22), green)
+b5 = Block(Vec2(6, 3), Vec2(44, 20), brown)
+b5_2 = Block(Vec2(6, 1), Vec2(44, 19), green)
 
-# Character wird initialisiert
-tom = Character()
+tom = Character(Vec2(10,400))
+
+
+gegner = Gegner(Vec2(50,300),Vec2(2,2),100, 600)
 
 # Spielschleife
 while True:
     # Screen wird bei jedem Schleifendurchlauf auf blaue Hintergrundfarbe resettet
     screen.fill((80,150,255))
+    gegner.move()
+    gegner.draw()
 
-    # Blöcke und Character werden bei jedem Schleifendruchlauf geupdatet und gedrawt
+
+    # Bloecke und Character werden bei jedem Schleifendruchlauf geupdatet und gedrawt
+
     tom.draw()
     b1.update()
     b1_2.update()
@@ -56,7 +63,7 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # Keybinds für Bewegung des Characters nach links (A) und nach rechts (D) festgelegt
+    # Keybinds fuer Bewegung des Characters nach links (A) und nach rechts (D) festgelegt
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]==False and keys[pygame.K_d]==False:
         tom.standstill()
@@ -64,12 +71,14 @@ while True:
         tom.moveleft()
     if keys[pygame.K_d]:
         tom.moveright()
+    if keys[pygame.K_w]:
+        tom.jump()
 
 
     dt = clock.get_time() / 1000.0 # Zeit seit dem letzten tick (Frame) in Sek.
     clock.tick(60) # Kontrolliert die Aktuallisierungen pro Minute (FPS)
 
-    # Ruft update auf CollisionManager auf -> bewegt HitBoxen, prüft Kollisionen
+    # Ruft update auf CollisionManager auf -> bewegt HitBoxen, prueft Kollisionen
     CollisionManager().update(dt)
 
     pygame.display.flip()
