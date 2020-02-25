88810b3a4bf89a1c4deb0cb690016c9504137cd5
MariusDoe <github.marius@doerbandt.de>
2020-02-25 02:49:09 -0700

    added imports added characterSpawn parameter in Character constructor added loadFile(name) added missing comma added dt parameter in object.update(dt) added draw()

--- Level.py
+++ Level.py
@@ -1,3 +1,8 @@
+import json
+from Character import *
+from Block import *
+from GegnerClass import *
+
 colors = {
     "brown": (150,80,50),
     "blue": (80,150,255),
@@ -8,11 +13,16 @@ class Level:
 
     def __init__(self, objects, characterSpawn):
         self.objects = objects
-        self.character = Character() # TODO: use characterSpawn
+        self.character = Character(characterSpawn) # TODO: use characterSpawn
         self.objects.append(self.character)
+
+    def loadFile(name):
+        file = open(name)
+        return Level.load(json.load(file))
 
     def load(json):
         objects = []
+        level = json["level"]
         for object in level["objects"]:
             if object["type"] == "block":
                 objects.append(Block(
@@ -24,7 +34,7 @@ class Level:
                 objects.append(Gegner(
                     Vec2(*object["position"]),
                     Vec2(*object["size"]),
-                    object["range"][0]
+                    object["range"][0],
                     object["range"][1]
                 ))
         characterSpawn = Vec2(*level["characterSpawn"])
@@ -32,5 +42,9 @@ class Level:
 
     def update(self, dt):
         for object in self.objects:
-            object.update()
+            object.update(dt)
         CollisionManager().update(dt)
+
+    def draw(self):
+        for object in self.objects:
+            object.draw()
