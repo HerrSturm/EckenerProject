b1f21712613d19928aa0649acf1f4687fe8536e5
LarsInformatik <lars.blindow@gmail.com>
2020-02-25 10:12:26 +0100

    Wir haben das Bild in der richtigen Skalierung eingefügt.

--- characterClass.py
+++ characterClass.py
@@ -2,13 +2,16 @@ import pygame
 from HitBox import*
 class Character():
     def __init__(self):
-        self.hitBox = HitBox(Vec2(50,50), Vec2(150,150), False, Layer("player"), Vec2(0.5, 0))
+        self.image = pygame.image.load("Graphics/aAllGraphics/Adventurer/adventurer-idle-00.png").convert_alpha()
+        self.imagebig = pygame.transform.scale(self.image, (125, 75))
+        #self.run = ["adventurer-run-00.png","adventurer-run-01.png","adventurer-run-02.png",
+                #"adventurer-run-03.png","adventurer-run-04.png","adventurer-run-05.png"]
+        self.hitBox = HitBox(Vec2(50,50), Vec2(125, 75), False, Layer("player"), Vec2(0.5, 0))
         self.mainScreen = pygame.display.get_surface()
-        pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
+        #pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
     def draw(self):
-        pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
-
-
+        self.mainScreen.blit(self.imagebig, (self.hitBox.pos.x,self.hitBox.pos.y))
+        #pygame.draw.rect(self.mainScreen, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
     def moveright(self):                           #Funktion um die Hitbox nach rechts zu bewegen (geschw. auf +1)
         self.hitBox.vel = Vec2(300,0)            #hitbox bewegt sich nach rechts
 
