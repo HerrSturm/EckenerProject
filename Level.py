4d6879529a1281d4a1c37551231fbc0b46801ba2
MariusDoe <github.marius@doerbandt.de>
2020-02-25 03:28:00 -0700

    added camera

--- Level.py
+++ Level.py
@@ -2,6 +2,7 @@ import json
 from Character import *
 from Block import *
 from GegnerClass import *
+from Camera import *
 
 colors = {
     "brown": (150,80,50),
@@ -11,10 +12,11 @@ colors = {
 
 class Level:
 
-    def __init__(self, objects, characterSpawn):
+    def __init__(self, objects, characterSpawn, size, background):
         self.objects = objects
         self.character = Character(characterSpawn) # TODO: use characterSpawn
         self.objects.append(self.character)
+        self.camera = Camera(size, background)
 
     def loadFile(name):
         file = open(name)
@@ -38,7 +40,9 @@ class Level:
                     object["range"][1]
                 ))
         characterSpawn = Vec2(*level["characterSpawn"])
-        return Level(objects, characterSpawn)
+        size = Vec2(*level["size"])
+        background = tuple(level["background"])
+        return Level(objects, characterSpawn, size, background)
 
     def update(self, dt):
         for object in self.objects:
@@ -47,7 +51,8 @@ class Level:
 
     def draw(self):
         for object in self.objects:
-            object.draw()
+            object.draw(self.camera.surface)
+        self.camera.draw()
 
     def remove(self):
         for object in self.objects:
