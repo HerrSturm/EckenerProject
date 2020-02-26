a9526ead02858c70aecff93b95451ac096d73f17
MariusDoe <github.marius@doerbandt.de>
2020-02-26 01:14:34 -0700

    made the camera start at character

--- Level.py
+++ Level.py
@@ -17,6 +17,7 @@ class Level:
         self.character = Character(characterSpawn) # TODO: use characterSpawn
         self.objects.append(self.character)
         self.camera = Camera(size * 24, background)
+        self.camera.moveToCenter(self.character.hitBox.center)
 
     def loadFile(name):
         file = open(name)
