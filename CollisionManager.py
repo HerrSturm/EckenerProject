90e264accc4b27435408f97116406cb8fbaa7f44
MariusDoe <github.marius@doerbandt.de>
2020-03-03 03:00:13 -0700

    added layer parameter to onCollide callback

--- CollisionManager.py
+++ CollisionManager.py
@@ -29,34 +29,44 @@ class CollisionManager(object):
         def _physics(self, dt):
             self._callCallbacks("beforeUpdate", dt)
             for hitBox in self.hitBoxes:
-                if hitBox.static:
-                    continue
                 near = self.getNearHitBoxes(hitBox)
                 hitBox.pos.x += hitBox.vel.x * dt
                 for other in near:
-                    if not self.layerMap.matches(hitBox.layer, other.layer):
+                    trigger = hitBox.listensToCollisionLayer(other.layer)
+                    move = self.layerMap.matches(hitBox.layer, other.layer)
+                    if not (move or trigger):
+                        continue
+                    if hitBox.static:
                         continue
                     if hitBox.overlap(other):
                         if hitBox.right > other.left and hitBox.vel.x > 0:
-                            hitBox.right = other.left
-                            hitBox.vel.x = 0
+                            if move:
+                                hitBox.right = other.left
+                                hitBox.vel.x = 0
                             hitBox._collide(other, Direction.RIGHT)
                         if hitBox.left < other.right and hitBox.vel.x < 0:
-                            hitBox.left = other.right
-                            hitBox.vel.x = 0
+                            if move:
+                                hitBox.left = other.right
+                                hitBox.vel.x = 0
                             hitBox._collide(other, Direction.LEFT)
                 hitBox.pos.y += hitBox.vel.y * dt
                 for other in near:
-                    if not self.layerMap.matches(hitBox.layer, other.layer):
+                    trigger = hitBox.listensToCollisionLayer(other.layer)
+                    move = self.layerMap.matches(hitBox.layer, other.layer)
+                    if not (move or trigger):
+                        continue
+                    if hitBox.static:
                         continue
                     if hitBox.overlap(other):
                         if hitBox.bottom > other.top and hitBox.vel.y > 0:
-                            hitBox.bottom = other.top
-                            hitBox.vel.y = 0
+                            if move:
+                                hitBox.bottom = other.top
+                                hitBox.vel.y = 0
                             hitBox._collide(other, Direction.DOWN)
                         if hitBox.top < other.bottom and hitBox.vel.y < 0:
-                            hitBox.top = other.bottom
-                            hitBox.vel.y = 0
+                            if move:
+                                hitBox.top = other.bottom
+                                hitBox.vel.y = 0
                             hitBox._collide(other, Direction.UP)
             self._callCallbacks("afterUpdate", dt)
 
