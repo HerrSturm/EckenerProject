3c86931b011903947819ea01b8c3ae3473599240
Yannic <yannicbasin@yahoo.de>
2020-03-16 21:43:17 +0100

    Block zum Beenden eines Levels implementiert

--- Block.py
+++ Block.py
@@ -27,6 +27,26 @@ class Block(object):
     def move(self,position):
         self.position += position
 
+class EndBlock(object):
+    def __init__(self, position, size, color):
+        self.size = size * 24
+        self.position = position * 24
+        self.color = color
+        self.screen = pygame.display.get_surface()
+        self.hitbox = HitBox(self.position,self.size, True, Layer("end"))
+
+    def update(self, dt):
+        pass
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, [self.position.x,self.position.y, self.size.x,self.size.y])
+
+    def remove(self):
+        self.hitbox.remove()
+
+    def move(self,position):
+        self.position += position
+
     #def setvelocity(self, velocity):
     #    self.hitbox.vel = velocity
     #    pygame.draw.rect(self.screen, self.color, [self.position.x,self.position.y, self.size.x,self.size.y])
