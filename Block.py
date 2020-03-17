8a8c26ecda8f5dbf2aab7f1a1d3a5d9f8389f5c3
Yannic <yannicbasin@yahoo.de>
2020-03-17 17:26:41 +0100

    Vorläufig wurden Grastexturen für jeden Block eingeführt.

--- Block.py
+++ Block.py
@@ -7,19 +7,24 @@ from Vec2 import Vec2
 from HitBox import *
 
 class Block(object):
-    def __init__(self, position, size, color):
+    def __init__(self, position, size, color, graphicID = "grass"):
         super(Block, self).__init__()
         self.size = size * 24
         self.position = position * 24
         self.color = color
         self.screen = pygame.display.get_surface()
         self.hitbox = HitBox(self.position,self.size, True, Layer("solid"))
+        self.graphic = pygame.image.load("Graphics/Blocks/" + graphicID + ".png").convert_alpha()
+        self.graphic = pygame.transform.scale(self.graphic, (24, 24))
 
     def update(self, dt):
         pass
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, [self.position.x,self.position.y, self.size.x,self.size.y])
+        for x in range(0, int(self.size.x / 24)):
+            for y in range(0, int(self.size.y / 24)):
+                surface.blit(self.graphic, (self.position.x + x*24, self.position.y + y*24))
 
     def remove(self):
         self.hitbox.remove()
