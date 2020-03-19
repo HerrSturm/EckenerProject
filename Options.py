2d50103104d4e1004f2f20afbd005f8748e679e1
Marius Dörbandt <git.marius@doerbandt.de>
2020-03-19 12:53:40 +0100

    added DeathScreen back in added Options screen added transition from RESTART to DEATH_SCREEN added transition from NEXT_LEVEL to MAIN_MENU changed type checking from not == to != added level name display in DeathScreen and Menu adjusted display of text in menus changed text in Options to "Back" and "TODO"

--- Options.py
+++ Options.py
@@ -1,25 +1,27 @@
 import pygame, sys
-pygame.init()
+from GameState import GameState
+
 class Options(object):
     def __init__(self):
         self.mainScreen = pygame.display.get_surface()
-        pygame.draw.rect(self.mainScreen, (255, 255, 255), (440, 300, 400, 50))
-        pygame.draw.rect(self.mainScreen, (255, 255, 255), (440, 400, 400, 50))
-        pygame.draw.rect(self.mainScreen, (255, 255, 255), (440, 500, 400, 50))
+
     def draw(self):
         titleFont=pygame.font.SysFont('Arial Black',70)
         buttonFont=pygame.font.SysFont('Chandas', 30)
         titleText= titleFont.render("Options", False, (255, 255 ,255))
-        textButton1= buttonFont.render("Play", False, (0, 0 ,0))
-        textButton2= buttonFont.render("Options", False, (0, 0 ,0))
+        textButton1= buttonFont.render("Back", False, (0, 0 ,0))
+        textButton2= buttonFont.render("TODO", False, (0, 0 ,0))
         textButton3= buttonFont.render("Quit Game", False, (0, 0 ,0))
         self.mainScreen.blit(titleText,(450,100))
-        self.mainScreen.blit(textButton1,(595, 290))
-        self.mainScreen.blit(textButton2,(580, 390))
-        self.mainScreen.blit(textButton3,(560, 490))
+        pygame.draw.rect(self.mainScreen, (255, 255, 255), (440, 300, 400, 50))
+        pygame.draw.rect(self.mainScreen, (255, 255, 255), (440, 400, 400, 50))
+        pygame.draw.rect(self.mainScreen, (255, 255, 255), (440, 500, 400, 50))
+        self.mainScreen.blit(textButton1,(595, 320))
+        self.mainScreen.blit(textButton2,(580, 420))
+        self.mainScreen.blit(textButton3,(560, 520))
         pygame.display.flip()
 
-    def update(self):
+    def update(self, game):
         pygame.display.flip()
         coordinates = 1
         pygame.event.get()
@@ -27,7 +29,7 @@ class Options(object):
         pygame.mouse.get_pos()[coordinates]
         if 440 <= pygame.mouse.get_pos()[0] and pygame.mouse.get_pos()[0] <= 840 and 300 <= pygame.mouse.get_pos()[1] and 350 >= pygame.mouse.get_pos()[1]:
             if lMouse:
-                pass
+                game.state = GameState.MAIN_MENU
         if 440 <= pygame.mouse.get_pos()[0] and pygame.mouse.get_pos()[0] <= 840 and 400 <= pygame.mouse.get_pos()[1] and 450 >= pygame.mouse.get_pos()[1]:
             if lMouse:
                 pass
