4f2a394a20156057e71c363e5f38806afe36017a
Marius Dörbandt <git.marius@doerbandt.de>
2020-03-20 18:47:45 +0100

    added new levels to Game instance

--- tester.py
+++ tester.py
@@ -2,4 +2,5 @@ import pygame
 from Game import Game
 
 pygame.init()
-game = Game(["level01.json", "level02.json", "level03.json"])
+game = Game(["level01.json", "level02.json", "level03.json",
+             "level04.json", "level05.json", "level06.json"])
