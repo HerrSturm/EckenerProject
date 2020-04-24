7ed533035938582f0142bbea78019f6a4a7998e2
DefinitivNichtFabius <fabsius2003@gmail.com>
2020-04-24 14:40:49 +0200

    Aus irgendeinem Grund hat er beim merge die GegnerClass Datei auf FollowingEnemeyBranch nicht geändert. GegnerClass aus GegnerBranch kopiert und in GegnerClass im FollowingEnemeyBranch engefügt.

--- GegnerClass.py
+++ GegnerClass.py
@@ -1,21 +1,9 @@
 import pygame, HitBox, Vec2
 from HitBox import *
 
-<<<<<<< HEAD
-LaufAnimationEye = [pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight1.png"),
-                    pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight2.png"),
-                    pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight3.png")]
-
-LaufAnimationChicken = [pygame.image.load("Graphics/EnemyGraphics/chicken/ausgeschnitten/chicken1.png"),
-                    pygame.image.load("Graphics/EnemyGraphics/chicken/ausgeschnitten/chicken2.png"),
-                    pygame.image.load("Graphics/EnemyGraphics/chicken/ausgeschnitten/chicken3.png"),
-                    pygame.image.load("Graphics/EnemyGraphics/chicken/ausgeschnitten/chicken4.png")]
-=======
->>>>>>> master
 
 class Gegner(object):
     def __init__(self, pos, size, startRange, endRange):
-        global LaufAnimationEye
         pos *= 24
         pos.y -= 8
         size *= 24
@@ -24,12 +12,6 @@ class Gegner(object):
         self.endRange = pos.x + endRange * 24
         self.frame = 0
         self.mainScreen = pygame.display.get_surface()
-<<<<<<< HEAD
-        self.LaufAnimationRight = LaufAnimationEye
-        self.enemy = self.LaufAnimationRight[0].convert_alpha()
-        self.img = 0
-
-=======
 
         #self.enemy = [
             #pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight1.png").convert_alpha(),
@@ -45,7 +27,6 @@ class Gegner(object):
                             pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Goblin/GoblinFrames/runRight/frame6.png"), (70, 55)),
                             pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Goblin/GoblinFrames/runRight/frame7.png"), (70, 55)),
                             pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Goblin/GoblinFrames/runRight/frame8.png"), (70, 55))]
->>>>>>> master
 
     #führt move Methode wiederholt durch
     def update(self, game, dt):
@@ -57,15 +38,7 @@ class Gegner(object):
     #legt Grafik für Gegner fest
     def draw(self,surface):
         #pygame.draw.rect(surface, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
-<<<<<<< HEAD
-<<<<<<< HEAD
-        self.enemy = self.LaufAnimationRight[(self.frame//50)%len(self.LaufAnimationRight)-1].convert_alpha()
-=======
-        self.enemy = self.LaufAnimationGoblin[(self.frame//50)%len(self.LaufAnimationGoblin)-2]
->>>>>>> master
-=======
         self.enemy = self.LaufAnimationGoblin[(self.frame//50)%len(self.LaufAnimationGoblin)-4]
->>>>>>> master
         if self.hitBox.vel.x < 0:
             self.enemy = pygame.transform.flip(self.enemy,True,False)
         surface.blit(self.enemy,((self.hitBox.pos.x) -8,self.hitBox.pos.y))
@@ -90,33 +63,9 @@ class Gegner(object):
             self.frame = self.frame + 4
             if self.hitBox.pos.x < self.endRange:
                 self.hitBox.vel.x = 100
-<<<<<<< HEAD
-<<<<<<< HEAD
-            self.frame = self.frame + 1
-
-
-class TeleportingChicken(Gegner):
-    def __init__(self, pos, size, startRange, endRange):
-        global LaufAnimationChicken
-        super().__init__(pos, size, startRange, endRange)
-        self.LaufAnimationRight = LaufAnimationChicken
-
-
-    def move(self):
-        if self.hitBox.vel.x == 0:
-            self.hitBox.vel.x = 100
-        if self.startRange < self.endRange:
-            if self.hitBox.pos.x > self.endRange:
-                self.hitBox.pos.x = self.startRange
-            self.frame = self.frame + 5
-=======
-            self.frame = self.frame + 2
->>>>>>> master
-=======
             self.frame = self.frame + 4
             if self.hitBox.vel.x == 0:
                 if self.hitBox.pos.x > game.currentLevel.character.hitBox.pos.x:
                     self.hitBox.vel.x = 100
                 if self.hitBox.pos.x < game.currentLevel.character.hitBox.pos.x:
                     self.hitBox.vel.x = -100
->>>>>>> master
