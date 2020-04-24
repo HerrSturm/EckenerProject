d2146d9ffda4b635a724a2db75a926cf914ad34d
DefinitivNichtFabius <fabsius2003@gmail.com>
2020-04-24 17:31:48 +0200

    Grafik in Feldermaus geändert und Hitbox angepasst. Bug mit Spawnposition muss noch behoben werden.

--- FollowingEnemy.py
+++ FollowingEnemy.py
@@ -5,13 +5,16 @@ from HitBox import *
 class FollowingEnemy(Gegner):
     def __init__(self, pos, size, startrange, endrange):
         super().__init__(pos, size, startrange, endrange)
-        self.FlugAnimationObserver = [pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight1.png"), (70, 55)),
-                            pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight2.png"), (70, 55)),
-                            pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/observer/AnimationRechts/observerRight3.png"), (70, 55))]
+        #pos.y += 400
+        self.hitBox = HitBox(pos, Vec2(55,40), False, Layer("deadly"), Vec2(100,0))
+        self.FlugAnimationBat = [pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame1.png"), (120, 105)),
+                            pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame2.png"), (120, 105)),
+                            pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame3.png"), (120, 105)),
+                            pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame4.png"), (120, 105))]
 
     def draw(self,surface):
-        #pygame.draw.rect(surface, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
-        self.enemy = self.FlugAnimationObserver[(self.frame//50)%len(self.FlugAnimationObserver)-3]
+        pygame.draw.rect(surface, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
+        self.enemy = self.FlugAnimationBat[(self.frame//50)%len(self.FlugAnimationBat)-4]
         if self.hitBox.vel.x < 0:
             self.enemy = pygame.transform.flip(self.enemy,True,False)
-        surface.blit(self.enemy,((self.hitBox.pos.x) -8,self.hitBox.pos.y))
+        surface.blit(self.enemy,(self.hitBox.pos.x -34,self.hitBox.pos.y -30))
