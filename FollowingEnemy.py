5fa5531f1eaf83d991a6470f2c977af392c1b2c3
DefinitivNichtFabius <fabsius2003@gmail.com>
2020-04-26 11:54:58 +0200

    HitBox transparent gemacht. Unsichtbare zweite Hitbox bug gefixt.

--- FollowingEnemy.py
+++ FollowingEnemy.py
@@ -5,14 +5,14 @@ from HitBox import *
 class FollowingEnemy(Gegner):
     def __init__(self, pos, size, startrange, endrange):
         super().__init__(pos, size, startrange, endrange)
-        self.hitBox = HitBox(pos*24, Vec2(53,40), False, Layer("deadly"), Vec2(100,0))
+        self.hitBox.size = Vec2(53,40)
         self.FlugAnimationBat = [pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame1.png"), (120, 105)),
                             pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame2.png"), (120, 105)),
                             pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame3.png"), (120, 105)),
                             pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/Bat/BatFrames/BatFrame4.png"), (120, 105))]
 
     def draw(self,surface):
-        pygame.draw.rect(surface, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
+        #pygame.draw.rect(surface, (255, 255, 255), (self.hitBox.pos.values[0], self.hitBox.pos.values[1], self.hitBox.size.values[0], self.hitBox.size.values[1]))
         self.enemy = self.FlugAnimationBat[(self.frame//50)%len(self.FlugAnimationBat)-4]
         if self.hitBox.vel.x < 0:
             self.enemy = pygame.transform.flip(self.enemy,True,False)
