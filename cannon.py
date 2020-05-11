b43057576af71de3b888c73c7f7c1fc6eeb89bd6
DefinitivNichtFabius <fabsius2003@gmail.com>
2020-05-11 12:37:53 +0200

    Respawn funktioniert bedingt (startposition ändert sich laufend).

--- cannon.py
+++ cannon.py
@@ -1,6 +1,7 @@
 import pygame, HitBox, Vec2
 from GegnerClass import *
 from HitBox import *
+from Character import *
 
 #Cannon Klasse durch Oberklasse erstellt
 class Cannon(object):
@@ -32,12 +33,13 @@ class CannonBall(object):
     def __init__(self, pos, size, startRange, endRange):
         pos *= 24
         size *= 24
-        pos.x -= 50
-        pos.y += 10
-        self.hitBox = HitBox(pos, Vec2(31,31), False, Layer("Deadly"))
-        self.startRange = pos.x + startRange * 24
-        self.endRange = pos.x + endRange * 24
-        self.halfRange = (self.endRange/2)
+        #pos.x -= 50
+        #pos.y += 10
+        self.startPos = pos
+        self.hitBox = HitBox(pos, Vec2(31,31), False, Layer("deadly"))
+        self.startRange = startRange * 24
+        self.endRange = endRange * 24
+        self.halfRange = (self.startRange/2 + self.endRange/2)
         self.mainScreen = pygame.display.get_surface()
         self.img = 0
         self.cannonBallGraphic = pygame.transform.scale(pygame.image.load("Graphics/EnemyGraphics/cannon/cannonBallTrans.png"), (45, 35))
@@ -55,11 +57,16 @@ class CannonBall(object):
         surface.blit(self.enemy,((self.hitBox.pos.x) -8,(self.hitBox.pos.y) -3))
 
     def move(self, game):
+        #print(self.startRange, self.halfRange, self.endRange)
+        #print(game.currentLevel.character.hitBox.pos.x)
+        #print(self.hitBox.vel.x)
+        print(self.startPos)
         if self.hitBox.pos.x > self.halfRange:
             self.hitBox.vel = Vec2(-100, -100)
-            #print("1")
-        if self.hitBox.pos.x < self.halfRange:
+            #print("up")
+        if self.hitBox.pos.x < self.halfRange and self.hitBox.pos.x > self.endRange:
             self.hitBox.vel = Vec2(-100, 100)
-            #print("2")
-        #if self.hitBox.pos.x < self.startRange:
-            #print("lol")
+            #print("down")
+        if self.hitBox.vel.x == 0 or self.hitBox.vel.y == 0:# or self.protection <= 0: #or self.hitBox.pos == game.currentLevel.character.hitBox.pos:
+            self.hitBox.pos = self.startPos
+            #print("respawn")
