ebe0db8e73d4eceac78741097a196a3ac217c464
HerrSturm <59501137+HerrSturm@users.noreply.github.com>
2020-06-12 09:48:10 +0200

    Delete sounds.py
    Datei erfüllt keinen Zweck, da der Quellcode auskommentiert ist.

--- sounds.py
+++
@@ -1,6 +0,0 @@
-#Hier befinden sich alle Sprites, die in der Animation verwendet werden sollen.
-import pygame
-#path = 'Sounds'
-#def slidingSound():
-#    sliding = pygame.mixer.Sound(path+"slidingSound.mp3")
-#    return sliding
