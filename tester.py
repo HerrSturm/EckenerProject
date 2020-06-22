94961f8a0172723a46b93f221bcfe0c288a74205
DefinitivNichtFabius <fabsius2003@gmail.com>
2020-06-22 10:44:17 +0200

    Level 10 und 11 hintugefügt

--- tester.py
+++ tester.py
@@ -2,7 +2,7 @@ import pygame
 from Game import Game
 
 pygame.init()
-game = Game(["level10.json", "level01.json", "level02.json",
+game = Game(["level00.json", "level01.json", "level02.json",
              "level03.json", "level04.json", "level05.json",
              "level06.json", "level07.json", "level08.json",
-             "level09.json"])
+             "level09.json", "level10.json", "level11.json"])
